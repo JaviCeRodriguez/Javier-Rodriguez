de20a008f4f952a193e39ee5108fb63adbf51548
JaviCeRodriguez <javicerodriguez@gmail.com>
2021-01-10 19:40:54 -0300

    ejercicio card courseit

---
+++ CourseIt/card/js/scriptCard.js
@@ -0,0 +1,7 @@
+const eidi = document.getElementById('btnMeet');
+const clase = document.getElementsByClassName('wrapper');
+const tags = document.getElementsByTagName('div');
+
+console.log(eidi);
+console.log(clase);
+console.log(tags);
